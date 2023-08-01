f54c9e2416c01e890b96bc2ba4cb51d5cead07af
aborel <alain.borel@epfl.ch>
2023-08-01 07:35:35 +0200

    first IJLLR scaffold commit, non-functional

---
+++ Indian Journal of Law and Legal Research.js
@@ -0,0 +1,18 @@
+{
+	"translatorID": "abca0ea9-1434-48ea-a04f-0d926973686b",
+	"label": "Indian Journal of Law and Legal Research",
+	"creator": "Alain Borel",
+	"target": "https://www.ijllr.com",
+	"minVersion": "5.0",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2023-08-01 05:34:46"
+}
+
+/** BEGIN TEST CASES **/
+var testCases = [
+]
+/** END TEST CASES **/
