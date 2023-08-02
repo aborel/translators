795bbc6f0611efd610ff8045eea4d5327b6e18cc
aborel <alain.borel@epfl.ch>
2023-08-02 06:56:07 +0200

    basic translator functions as placeholders

--- Indian Journal of Law and Legal Research.js
+++ Indian Journal of Law and Legal Research.js
@@ -12,7 +12,8 @@
 	"lastUpdated": "2023-08-01 05:34:46"
 }
 
-unction detectWeb(doc, url) {
+
+function detectWeb(doc, url) {
 	if (url.includes("volume")) {
 		return "multiple";
 	}
@@ -20,6 +21,48 @@ unction detectWeb(doc, url) {
 		return "journalArticle";
 	}
 	return false;
+}
+
+
+function getSearchResults(doc, checkOnly) {
+        var items = {};
+        var found = false;
+        // TODO adapt selector, some layers of spans might be required
+        var rows = doc.querySelectorAll('h4 > span');
+        for (let row of rows) {
+                // Zotero.debug(row.innerHTML);
+                let href = row.href;
+                // Zotero.debug(href);
+                let title = ZU.trimInternal(row.textContent);
+                if (!href || !title) continue;
+                if (checkOnly) return true;
+                found = true;
+                items[href] = title;
+        }
+        return found ? items : false;
+}
+
+
+async function doWeb(doc, url) {
+        if (detectWeb(doc, url) == 'journalArticle') {
+                await scrape(doc, url);
+        }
+        else if (detectWeb(doc, url) == 'multiple') {
+                let items = await Zotero.selectItems(getSearchResults(doc, false));
+                if (!items) return;
+                for (let url of Object.keys(items)) {
+                        await scrape(await requestDocument(url));
+                }
+        }
+        else {
+                // The fallback is not expected to be used on IJLLR, but just in case...
+                await scrape(doc, url);
+        }
+}
+
+
+async function scrape(nextDoc, url) {
+
 }
 
 /** BEGIN TEST CASES **/
